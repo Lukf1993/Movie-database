65dd89d286951f315c5bca454e46561c8042b5cd
Lukasz Fundament <lukf1993@gmail.com>
2019-10-17 12:20:11 +0200

    suggestion styled

--- src/components/AutoSuggestion.js
+++ src/components/AutoSuggestion.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-// import './App.css';
+import React from 'react';
+import './autoSuggestion.scss';
 
 const AutoSuggestion = props => {
 
@@ -11,13 +11,14 @@ const AutoSuggestion = props => {
         {suggestion.map((item, index) => (
             <>
             {index < 5  && (
-                <li>
-                    <button onClick={() => props.getMovie(item.id)}>
-                        <img src={`http://image.tmdb.org/t/p/w185/${item.poster_path}`} alt='poster'></img>
-                        <h1>{item.title}</h1>
-                        <p>{item.release_date}</p>
-                    </button>
-                    
+                <li key={item.id}>
+                    <button onClick={() => props.getMovie(item.id)} className='button'>
+                        <img src={`http://image.tmdb.org/t/p/w185/${item.poster_path}`} className='button_img' alt='poster'></img>
+                        <div className='flex'>
+                            <h1 className='button_title'>{item.title}</h1>
+                            <p className='button_paragraph'>{item.release_date}</p>
+                        </div>
+                    </button>                    
                 </li>
         )}
             </>
